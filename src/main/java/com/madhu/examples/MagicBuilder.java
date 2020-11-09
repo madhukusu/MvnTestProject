fa28dd23189f4d2e64393863990ba16a9f0ce862
Kusu <mkusu@ip-192-168-86-238.us-west-2.compute.internal>
2020-11-09 13:17:56 -0600

    first commit

---
+++ src/main/java/com/madhu/examples/MagicBuilder.java
@@ -0,0 +1,9 @@
+package com.madhu.examples;
+
+public class MagicBuilder {
+
+    public static int getLucky() {
+        return 7;
+    }
+
+}
---
+++ src/main/java/com/madhu/examples/MessageBuilder.java
@@ -0,0 +1,13 @@
+package com.madhu.examples;
+
+public class MessageBuilder {
+
+    public static String getHelloWorld(){
+        return "hello world";
+    }
+
+    public static int getNumber10(){
+        return 10;
+    }
+
+}
